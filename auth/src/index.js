b9155aaed4fbd01849b4134a892eb7e86c8c9844
Atif Shafi <shafi1@MacBook-Air.local>
2021-03-28 18:29:12 -0700

    Adding auth module

---
+++ auth/src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+    console.log('auth server started: 3000')
+});
+
+
+
