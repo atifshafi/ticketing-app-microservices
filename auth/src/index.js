aa3d7e150cdab40f92d7baac5ba70208b772ca66
Atif Shafi <shafi1@MacBook-Air.local>
2021-04-05 08:25:23 -0700

    Adding ingress yaml file

--- auth/src/index.js
+++ auth/src/index.js
@@ -4,8 +4,12 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send("Hello!");
+});
+
 app.listen(3000, () => {
-    console.log('auth server started: 3000')
+    console.log('auth server started: 3000!');
 });
 
 
