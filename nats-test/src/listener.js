f27c4326bade2a95e4f18461a14180c336132658
Atif Shafi <shafiatifbin@gmail.com>
2021-05-11 04:06:37 -0700

    Creating consumer/listener class

--- nats-test/src/listener.js
+++ nats-test/src/listener.js
@@ -17,35 +17,9 @@ stan.on('connect', () => {
         process.exit();
     });
 
-    // Defining configurable options
-    const options = stan
-        // Enabling queue group
-        .subscriptionOptions()
-        // By setting this, event bus will wait for acknowledgement from the service. If not received after some time, it will send again to another available instance
-        .setManualAckMode(true).
-        // This flag will tell the event bus to re-deliver all the messages when there's a disconnect between the client and the bus. Note that, this flag will re-send the messages which has been processed and acknowledged by the client if used by itself.
-        setDeliverAllAvailable().
-        // This flag helps event bus to keep track which messages has been processed (by using the provided id) so that upon re-connect of a service, event bus doesn't re-send messages which has been processed and acknowledged. Note that, it will only work with 'setDeliverAllAvailable' flag applied and queue group name added.
-        setDurableName('accounting-service')
+    // Call the listener class to start listening
+    new TicketCreatedListener(stan).listen();
 
-    // Name of the topic: ticket:created, queue group: example. Here, by having the same queue group for all the instances of 'listener' event bus, only one will process the message.
-    // As a result, only one message will be sent back to the services who are subscribed this queue group
-    const subscription = stan.subscribe(
-        'ticket:created',
-        'example',
-        options
-);
-
-    // Consuming the message
-    subscription.on('message', (msg) => {
-        console.log('Message received');
-        console.log(
-            'Message #' + msg.getSequence() + ', with data: ' + msg.getData()
-            );
-
-        // letting event bus know message has been processed
-        msg.ack();
-    })
 });
 
 // When the process is manually closed/interrupted. It will make sure the process closes the client first
@@ -53,57 +27,76 @@ stan.on('connect', () => {
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
 
-
-class listener {
-    constructor(client, qeueGroupName, subject) {
+// This class will be untouched for any topic
+class Listener {
+    constructor(client, queueGroupName, subject) {
+        // Pre-initialized NATS client
         this.client = client;
-        this.qeueGroupName = qeueGroupName
+        // Name of the queue group this listener will join
+        this.queueGroupName = queueGroupName
+        // Name of the topic
         this.subject = subject
+        // Number of seconds this listener has to ack a message
         this.ackWait = 5 * 1000;
-        this.onMessage(data, msg)
-        {
+        // Function to run when a message is received
+        this.onMessage = function (data, msg) {
         }
     }
 
+    // Default subscription option
     subscriptionOptions() {
         return this.client.
-            subscriptionOptions().
-            setDeliverAllAvailable().
-            setDurableName(this.qeueGroupName).
-            setManualAckMode(true).
-            setAckWait(this.ackWait)
+        subscriptionOptions().
+        setDeliverAllAvailable().
+        setDurableName(this.queueGroupName).
+        setManualAckMode(true).
+        setAckWait(this.ackWait)
     }
 
-    listen () {
+    // Cod to setup subscription
+    listen() {
         const subscription = this.client.subscribe(
             this.subject,
-            this.qeueGroupName,
+            this.queueGroupName,
             this.subscriptionOptions()
         );
 
         subscription.on('message', (msg) => {
-             console.log(
-            'Message Received: ' + this.subject  + '/' + this.qeueGroupName
-             );
+            console.log(
+                'Message Received: ' + this.subject + '/' + this.queueGroupName
+            );
 
             const parsedData = this.parseMessage(msg);
             this.onMessage(parsedData, msg);
         });
     }
 
+    // Helper function to parse a message
     parseMessage(msg) {
         const data = msg.getData();
         let parsed_data;
         if (typeof data === 'string') {
-             parsed_data = JSON.parse(data);
+            parsed_data = JSON.parse(data);
         } else {
             parsed_data = JSON.parse(data.toString('utf8'));
         }
         return parsed_data
 
 
+    }
+
 
 }
 
 
+// Update this function when needed
+class TicketCreatedListener extends Listener {
+    subject = 'ticket:created';
+    queueGroupName = 'payments-service';
+
+    onMessage = function (data, msg) {
+        console.log('event data!', data);
+
+        msg.ack();
+    }
 }
