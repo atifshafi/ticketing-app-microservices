0fe546c4eaa90818a6eea2d07f129d48c9b7e6ed
Atif Shafi <shafiatifbin@gmail.com>
2021-05-21 17:01:14 -0700

    adding new order logic

---
+++ orders/src/models/ticket.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const ticketSchema = new mongoose.Schema({
+        title: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: 0
+        }
+    },
+    // Format JSON body to send back on the response. 'transform' method is used to modify user document to a custom JSON
+    {
+        toJSON: {
+            transform(doc, ret) {
+                // Converting '_id' to 'id' to keep a consistent id reference
+                ret.id = ret._id;
+                delete ret._id;
+            }
+        }
+    }
+);
+
+
+
+const Ticket = mongoose.model('Ticket', ticketSchema);
+
+export {Ticket};
--- orders/src/routes/new.js
+++ orders/src/routes/new.js
@@ -1,6 +1,7 @@
 import express from "express";
-import {requireAuth, validateRequest} from '@atiftickets/common'
+import {requireAuth, validateRequest, NotFoundError} from '@atiftickets/common'
 import {body} from "express-validator";
+import {Ticket} from "../models/ticket.js"
 
 const route = express.Router();
 
@@ -9,6 +10,21 @@ route.post('/api/orders', requireAuth,
     body('ticketId').not().isEmpty().withMessage('TicketId must be provided'),
     validateRequest,
     async (req, res) => {
+
+        const {ticketId} = req.body;
+
+        // Find the ticket from DB
+        const ticket = await Ticket.findById(ticketId);
+        if (!ticket) {
+            throw new NotFoundError();
+        }
+
+        // Make sure the ticket is not already reserved
+
+        // Build the order and save it to the DB
+
+        // Publish an event saying that an order was created
+
     res.send({});
 
 });
